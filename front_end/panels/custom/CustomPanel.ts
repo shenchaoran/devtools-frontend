76fe8ed4ce86e56ebade97f167379c447d935822
chestershen <chestershen@tencent.com>
2021-08-03 11:35:47 +0800

    feat: custom tab 消息通知

--- front_end/panels/custom/CustomPanel.ts
+++ front_end/panels/custom/CustomPanel.ts
@@ -1,20 +1,30 @@
 import * as UI from '../../ui/legacy/legacy.js';
 
-
 let customPanelInstace: CustomPanel;
 export class CustomPanel extends UI.Panel.Panel {
   constructor() {
     super('custom');
 
-    let _contentElement =  document.createElement("div");
-    // 编写面板的具体内容
-    _contentElement.innerText = 'testtesttest';
-    this.element.appendChild(_contentElement);
+    const iframe = document.createElement('iframe');
+    iframe.setAttribute('src', '/extensions/memory.html');
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
+    iframe.style.overflow = 'auto';
+    this.contentElement.appendChild(iframe);
 
+    window.addEventListener(
+      'message',
+      (e) => {
+        if (e.data === 'getParentUrl') {
+          iframe.contentWindow?.postMessage(window.location.href, e.origin);
+        }
+      },
+      false,
+    );
   }
 
-  static instance(opts = {forceNew: null}): CustomPanel {
-    const {forceNew} = opts;
+  static instance(opts = { forceNew: null }): CustomPanel {
+    const { forceNew } = opts;
     if (!customPanelInstace || forceNew) {
       customPanelInstace = new CustomPanel();
     }
